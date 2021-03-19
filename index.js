d1fc6a6b0dd29f5572a9b76a902c8e4523018d23
ihorhub <ihor_babiy@ukr.net>
2021-03-20 01:26:53 +0200

    add new test svg

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('click',(event)=>{
-    event.preventDefault
-})
-console.log(z\[]]\';'';./\|)
+const logo = document.querySelectorAll('#logo path')
+for (let i = 0; i < logo.length; i++) {
+  console.log(`Letter ${i} is ${logo[i]}`)
+}
